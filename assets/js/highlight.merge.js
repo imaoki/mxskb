0e79d56c9db6d9e39fa8dc9b7ad8fae4c188217f
imaoki <imaoki117@gmail.com>
2019-08-25 21:20:18 +0900

    Added python definition to highlightjs

--- assets/js/highlight.merge.js
+++ assets/js/highlight.merge.js
@@ -1,4 +1,4 @@
-/*! highlight.js v9.15.6 | BSD3 License | git.io/hljslicense */
+/*! highlight.js v9.15.10 | BSD3 License | git.io/hljslicense */
 /*! © 2019 imaoki | MIT License | https://github.com/imaoki */
 /*
 hljs.registerLanguage("css", );
@@ -7,8 +7,10 @@ hljs.registerLanguage("ebnf", );
 hljs.registerLanguage("javascript", );
 hljs.registerLanguage("markdown", );
 hljs.registerLanguage("maxscript", );
+hljs.registerLanguage("python", );
 hljs.registerLanguage("xml", );
 */
+
 (function(factory) {
 
   // Find the global object for export to both the browser and web workers.
@@ -17,7 +19,8 @@ hljs.registerLanguage("xml", );
 
   // Setup highlight.js for different environments. First is Node.js or
   // CommonJS.
-  if(typeof exports !== 'undefined') {
+  // `nodeType` is checked to ensure that `exports` is not a HTML element.
+  if(typeof exports !== 'undefined' && !exports.nodeType) {
     factory(exports);
   } else if(globalObject) {
     // Export hljs globally even when using AMD for cases when this script
@@ -429,6 +432,7 @@ hljs.registerLanguage("xml", );
 
       openSpan += classname + '">';
 
+      if (!classname) return insideSpan;
       return openSpan + insideSpan + closeSpan;
     }
 
@@ -1440,6 +1444,122 @@ hljs.registerLanguage("maxscript", function(hljs) {
       mxsVerbatim,
       mxsName,
       mxsPathName
+    ]
+  };
+});
+hljs.registerLanguage("python", function(hljs) {
+  var KEYWORDS = {
+    keyword:
+      'and elif is global as in if from raise for except finally print import pass return ' +
+      'exec else break not with class assert yield try while continue del or def lambda ' +
+      'async await nonlocal|10',
+    built_in:
+      'Ellipsis NotImplemented',
+    literal: 'False None True'
+  };
+  var PROMPT = {
+    className: 'meta',  begin: /^(>>>|\.\.\.) /
+  };
+  var SUBST = {
+    className: 'subst',
+    begin: /\{/, end: /\}/,
+    keywords: KEYWORDS,
+    illegal: /#/
+  };
+  var STRING = {
+    className: 'string',
+    contains: [hljs.BACKSLASH_ESCAPE],
+    variants: [
+      {
+        begin: /(u|b)?r?'''/, end: /'''/,
+        contains: [hljs.BACKSLASH_ESCAPE, PROMPT],
+        relevance: 10
+      },
+      {
+        begin: /(u|b)?r?"""/, end: /"""/,
+        contains: [hljs.BACKSLASH_ESCAPE, PROMPT],
+        relevance: 10
+      },
+      {
+        begin: /(fr|rf|f)'''/, end: /'''/,
+        contains: [hljs.BACKSLASH_ESCAPE, PROMPT, SUBST]
+      },
+      {
+        begin: /(fr|rf|f)"""/, end: /"""/,
+        contains: [hljs.BACKSLASH_ESCAPE, PROMPT, SUBST]
+      },
+      {
+        begin: /(u|r|ur)'/, end: /'/,
+        relevance: 10
+      },
+      {
+        begin: /(u|r|ur)"/, end: /"/,
+        relevance: 10
+      },
+      {
+        begin: /(b|br)'/, end: /'/
+      },
+      {
+        begin: /(b|br)"/, end: /"/
+      },
+      {
+        begin: /(fr|rf|f)'/, end: /'/,
+        contains: [hljs.BACKSLASH_ESCAPE, SUBST]
+      },
+      {
+        begin: /(fr|rf|f)"/, end: /"/,
+        contains: [hljs.BACKSLASH_ESCAPE, SUBST]
+      },
+      hljs.APOS_STRING_MODE,
+      hljs.QUOTE_STRING_MODE
+    ]
+  };
+  var NUMBER = {
+    className: 'number', relevance: 0,
+    variants: [
+      {begin: hljs.BINARY_NUMBER_RE + '[lLjJ]?'},
+      {begin: '\\b(0o[0-7]+)[lLjJ]?'},
+      {begin: hljs.C_NUMBER_RE + '[lLjJ]?'}
+    ]
+  };
+  var PARAMS = {
+    className: 'params',
+    begin: /\(/, end: /\)/,
+    contains: ['self', PROMPT, NUMBER, STRING]
+  };
+  SUBST.contains = [STRING, NUMBER, PROMPT];
+  return {
+    aliases: ['py', 'gyp', 'ipython'],
+    keywords: KEYWORDS,
+    illegal: /(<\/|->|\?)|=>/,
+    contains: [
+      PROMPT,
+      NUMBER,
+      STRING,
+      hljs.HASH_COMMENT_MODE,
+      {
+        variants: [
+          {className: 'function', beginKeywords: 'def'},
+          {className: 'class', beginKeywords: 'class'}
+        ],
+        end: /:/,
+        illegal: /[${=;\n,]/,
+        contains: [
+          hljs.UNDERSCORE_TITLE_MODE,
+          PARAMS,
+          {
+            begin: /->/, endsWithParent: true,
+            keywords: 'None'
+          }
+        ]
+      },
+      {
+        className: 'meta',
+        begin: /^[\t ]*@/, end: /$/
+      },
+      {
+        begin: /\b(print|exec)\(/ // don’t highlight keywords-turned-functions in Python 3
+      }
     ]
   };
 });
